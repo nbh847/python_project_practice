e844a7071f6763f3a3990211e990e89ac4d1f27e
weasny <weasny@192.168.0.100>
2018-06-02 23:12:30 +0800

    add data to localhost

--- python_client_server/client.py
+++ python_client_server/client.py
@@ -3,23 +3,29 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('www.sina.com.cn', 80))
+s.connect(('127.0.0.1', 9999))
 
-# 发送数据
-s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
-
-# 接受数据
-buffer = []
-while True:
-    d = s.recv(1024)
-    if d:
-        buffer.append(d)
-    else:
-        break
-data = b''.join(buffer)
-
-# 关闭连接
+# 接收欢迎消息:
+print(s.recv(1024).decode('utf-8'))
+for data in [b'Michel', b'Tracy', b'Sarah']:
+    # 发送数据
+    s.send(data)
+    print (s.recv(1024).decode('utf-8'))
+s.send(b'exit')
 s.close()
-
-header, html = data.split(b'\r\n\r\n', 1)
-print(header.decode('utf-8'))
+#
+# # 接受数据
+# buffer = []
+# while True:
+#     d = s.recv(1024)
+#     if d:
+#         buffer.append(d)
+#     else:
+#         break
+# data = b''.join(buffer)
+#
+# # 关闭连接
+# s.close()
+#
+# header, html = data.split(b'\r\n\r\n', 1)
+# print(header.decode('utf-8'))
